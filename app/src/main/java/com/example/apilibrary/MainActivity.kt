cb8de3c885e2c000763d3ee6b1152ba04a5f922c
Devanand <devanand.dharashive@einfochips.com>
2022-02-09 13:32:53 +0530

    Hello

--- app/src/main/java/com/example/apilibrary/MainActivity.kt
+++ app/src/main/java/com/example/apilibrary/MainActivity.kt
@@ -7,74 +7,26 @@ import com.example.apilibrary.api.PostService
 import com.example.apilibrary.db.PostDatabase
 import com.example.apilibrary.models.CommentListItem
 import com.example.apilibrary.models.PostListItem
+import kotlinx.coroutines.runBlocking
+import retrofit2.http.Url
 
 //import java.lang.reflect.Array.get
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var postService: PostService
-    lateinit var postDatabase: PostDatabase
+//    lateinit var postService: PostService
+//    lateinit var postDatabase: PostDatabase
+    lateinit var postRepository: PostRepository
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val postsLiveData = MutableLiveData<List<PostListItem>>()   // publically not access
-        val postsLiveData1 = MutableLiveData<List<CommentListItem>>()   // publically not access
-
-        //val posts : LiveData<PostListItem>
-        val posts: MutableLiveData<List<PostListItem>>
-        // get() = postsLiveData
-
-        //get() = postsLiveData
-
-        val posts1: MutableLiveData<List<CommentListItem>>
-        //get() = postsLiveData1
-        //get() = postsLiveData1
-
-        suspend fun getPosts1(url1: String) {
-            // postService.getPosts(url1)
-
-            val result = postService.getPosts(url1)
-
-            if (result != null) {
-                for (postlistitem1 in result) {
-                    postDatabase.postDao().addtPost(result)  // save data in room database
-                    // Log.e("postDatabase", result.toString())
-
-                    postsLiveData.postValue(result)
-                }
-            }
-
-            //  }
-            //else{
-
-            // for Offline support. get data from room database & display on UI
-            val posts = postDatabase.postDao().getPosts()
-            postsLiveData.postValue(posts)
-
-        }
-
-
-        suspend fun getPost2(url2 : String) {
-
-            val commentResult = postService.getComments(url2)
-
-            if (commentResult != null) {
-                for (postlistitem2 in commentResult) {
-                    postDatabase.postDao().addtComment(commentResult)  // save data in room database
-                    // Log.e("postDatabase", result.toString())
-
-                    postsLiveData1.postValue(commentResult)
-                }
-            } else {
-
-                // for Offline support. get data from room database & display on UI
-                val posts1 = postDatabase.postDao().getComments()
-                postsLiveData1.postValue(posts1)
-
-            }
+        //postRepository.getPosts1(url1 = "xsds")
+        val url : Unit
+        runBlocking {
+             url = postRepository.getPosts1("/posts")
         }
 
     }
